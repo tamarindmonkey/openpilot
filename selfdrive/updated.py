244b517effae3d0f34f62c3aa132bafd7d0b7ee7
cram <34961432+tamarindmonkey@users.noreply.github.com>
2022-09-27 10:21:29 -0400

    Update updated.py

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -52,8 +52,8 @@ OVERLAY_METADATA = os.path.join(STAGING_ROOT, "metadata")
 OVERLAY_MERGED = os.path.join(STAGING_ROOT, "merged")
 FINALIZED = os.path.join(STAGING_ROOT, "finalized")
 
-DAYS_NO_CONNECTIVITY_MAX = 14     # do not allow to engage after this many days
-DAYS_NO_CONNECTIVITY_PROMPT = 10  # send an offroad prompt after this many days
+DAYS_NO_CONNECTIVITY_MAX = 731     # do not allow to engage after this many days
+DAYS_NO_CONNECTIVITY_PROMPT = 730  # send an offroad prompt after this many days
 
 class WaitTimeHelper:
   def __init__(self, proc):
